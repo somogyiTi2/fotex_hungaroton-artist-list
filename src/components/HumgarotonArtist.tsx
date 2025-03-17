f06490f1adb87f13aa6ad109f5974e17be2ed86c
Somogyi Tibor <somogyi.tibor1997@gmail.com>
2025-03-17 10:23:52 +0100

    bugfix/fix the next page button

--- src/components/HumgarotonArtist.tsx
+++ src/components/HumgarotonArtist.tsx
@@ -8,12 +8,12 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { Box, Checkbox, FormControlLabel, MenuItem, TextField } from "@mui/material";
 
-
 const ArtistList = () => {
   const router = useRouter();
   const { query } = router;
-  const [page, setPage] = useState<number>(query.page ? parseInt(query.page as string) : 1);
+  const [page, setPage] = useState<number>(1);
   const [filters, setFilters] = useState<FetchArtistsParams>({ ...query, page });
+  console.log(page,filters)
 
   const [artists, setArtists] = useState<ResponseArtists[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
@@ -38,31 +38,35 @@ const ArtistList = () => {
       if (letter) newFilters.letter = letter;
       if (type as FetchArtistsParams["type"]) newFilters.type = type;
       if (includeImage) newFilters.include_image = includeImage;
+      
     }
 
     setFilters(newFilters);
     setPage(1);
   }
-
+  useEffect(() => {
+    setFilters((prevFilters) => ({ ...prevFilters, page })); // Mindig frissítjük a page-t a filterekben is
+  }, [page]);
 
   useEffect(() => {
     const getArtists = async () => {
       setLoading(true);
+      setError(null);
       try {
         const data = await fetchArtists(filters);
         setArtists(data.data || []);
-        router.push({
-          pathname: router.pathname,
-          query: { ...filters, page },
-        }, undefined, { shallow: true });
-        setError("")
+        router.push(
+          {
+            pathname: router.pathname,
+            query: { ...filters, page },
+          }, undefined,{ shallow: true });
       } catch (error) {
         console.error("Error fetching artists:", error);
-        setError(error)
+        setError(error);
       }
       setLoading(false);
     };
-
+  
     getArtists();
   }, [filters]);
 
@@ -74,20 +78,17 @@ const ArtistList = () => {
     setPage(1);
     router.push({
       pathname: router.pathname,
-      query: { page: 1 },
+      query: { page: 1 }
     });
   }
 
   return (
     <div className="margin-top-10">
-      <Button id="phoneFilter" variant="contained" onClick={() => setShowFilter(!showFilter)}>{showFilter ? <>⬆️Collapse Filters⬆️</> : <>⬇️Expand filters⬇️</>}</Button>
-      {showFilter === true &&
-        <Box
-          component="form"
-          onSubmit={handleSave}
-          ref={customForm}
-          autoComplete="off"
-        >
+      <Button id="phoneFilter" variant="contained" onClick={() => setShowFilter(!showFilter)}>
+        {showFilter ? <>⬆️ Collapse Filters ⬆️</> : <>⬇️ Expand Filters ⬇️</>}
+      </Button>
+      {showFilter &&
+        <Box component="form" onSubmit={handleSave} ref={customForm} autoComplete="off">
           <TextField
             className="margin-top-10"
             id="search"
@@ -122,36 +123,26 @@ const ArtistList = () => {
             <MenuItem value="is_primary">Primary</MenuItem>
           </TextField>
 
-
           <FormControlLabel
             label="Include Image"
-            control={
-              <Checkbox
-                id="include_image"
-                defaultChecked={query.include_image === "true"}
-              />
-            }
-
+            control={<Checkbox id="include_image" defaultChecked={query.include_image === "true"} />}
           />
 
           <Stack spacing={2} direction="row" className="flex-center">
             <Button type="submit" variant="contained">🔎 Filter</Button>
             <Button variant="contained" onClick={cleanHandler}>🧽 Clear</Button>
           </Stack>
-
         </Box>
       }
       <div className="margin-top-15">
         {error && <p>{error.message}</p>}
-
         {loading ? (
           <p>Loading...</p>
         ) : artists.length === 0 ? (
           <p>No artists found.</p>
         ) : (
-          <Table artists={artists} key="0" />
+          <Table artists={artists} />
         )}
-
 
         <Stack spacing={2} direction="row" className="flex-center margin-top-15">
           {page > 1 && (
@@ -163,7 +154,7 @@ const ArtistList = () => {
             </Button>
           )}
           <Button variant="contained">{page}</Button>
-          {artists.length !== 0 &&
+          {artists.length >= 50 &&
             <Button
               variant="outlined"
               onClick={() => setPage((prev) => prev + 1)}
