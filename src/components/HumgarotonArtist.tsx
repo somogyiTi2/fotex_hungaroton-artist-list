523bfd1b12287bdef1b04b7152c509ee5be094bd
Somogyi Tibor <somogyi.tibor1997@gmail.com>
2025-03-17 08:58:18 +0100

    feture/create filters

--- src/components/HumgarotonArtist.tsx
+++ src/components/HumgarotonArtist.tsx
@@ -1,34 +1,92 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { fetchArtists } from "@/pages/api/api";
-
-interface Artist {
-  id: number;
-  name: string;
-  portrait?: string;
-  albumCount: number;
-}
+import { useRouter } from "next/router";
+import { FetchArtistsParams } from "@/type/FetchArtistsParams";
+import { ResponseArtists } from "@/type/ResponseArtists";
 
 const ArtistList = () => {
-  const [artists, setArtists] = useState<Artist[]>([]);
+  const router = useRouter();
+  const { query } = router;
+  const [page, setPage] = useState<number>(query.page ? parseInt(query.page as string) : 1);
+  const [filters, setFilters] = useState<FetchArtistsParams>({ ...query, page });
+
+  const [artists, setArtists] = useState<ResponseArtists[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const [page, setPage] = useState<number>(1);
+  const customForm = useRef<HTMLFormElement>(null);
+
+  function handleSave(event: React.FormEvent) {
+    event.preventDefault();
+    const form = customForm.current;
+    const newFilters: FetchArtistsParams = { page: 1 }; 
+
+    if (form) {
+
+      const search = (form.elements.namedItem('search') as HTMLInputElement)?.value.trim();
+      const letter = (form.elements.namedItem('letter') as HTMLInputElement)?.value.trim();
+      const type = (form.elements.namedItem('type') as HTMLInputElement)?.value;
+      const includeImage = (form.elements.namedItem('include_image') as HTMLInputElement)?.checked;
+
+      if (search) newFilters.search = search;
+      if (letter) newFilters.letter = letter;
+      if (type as FetchArtistsParams["type"]) newFilters.type = type;
+      if (includeImage) newFilters.include_image = includeImage;
+    }
+
+    setFilters(newFilters);
+    setPage(1); 
+  }
+
 
   useEffect(() => {
     const getArtists = async () => {
       setLoading(true);
       try {
-        const data = await fetchArtists({ page });
+        const data = await fetchArtists(filters);
         setArtists(data.data || []);
+        router.push({
+          pathname: router.pathname,
+          query: { ...filters, page },
+        }, undefined, { shallow: true });   
       } catch (error) {
         console.error("Error fetching artists:", error);
       }
       setLoading(false);
     };
+
     getArtists();
-  }, [page]);
+  }, [filters]);
 
   return (
     <div>
+      <form onSubmit={handleSave} ref={customForm}>
+        <label>
+          Search
+          <input type="text" id="search" defaultValue={query.search ? query.search as string : ""} />
+        </label>
+        <label>
+          Letter
+          <input type="text" id="letter" maxLength={1} defaultValue={query.letter ? query.letter as string : ""} />
+        </label>
+        <label>
+          Type
+          <select id="type" defaultValue={query.type ? query.type as string : ""}>
+            <option value="">Select type</option>
+            <option value="is_composer">Composer</option>
+            <option value="is_performer">Performer</option>
+          </select>
+        </label>
+        <label>
+          Include Image
+          <input
+            type="checkbox"
+            name="include_image"
+            id="include_image"
+            defaultChecked={query.include_image === "true"}
+          />
+        </label>
+        <button>🔎 Filter</button>
+      </form>
+
       {loading ? <p>Loading...</p> : artists.map((artist) => (
         <div key={artist?.id}>
           {artist.portrait &&
@@ -38,7 +96,12 @@ const ArtistList = () => {
           <p>Albumok száma: {artist.albumCount}</p>
         </div>
       ))}
-      <button onClick={() => setPage((prev) => prev + 1)}>Következő oldal</button>
+
+      <div>
+        {page > 1 && <button onClick={() => setPage((prev) => prev - 1)}>{page - 1}</button>}
+        {page}
+        <button onClick={() => setPage((prev) => prev + 1)}>{page + 1}</button>
+      </div>
     </div>
   );
 };
